1bdf84a51a9645c00ceaec563e0050eb2453a42a
heesuchoi1 <heesu7563@gmail.com>
2014-09-11 14:21:22 +0900

    iuyrewqryuopoii

--- sketch.js
+++ sketch.js
@@ -8,12 +8,12 @@ function setup() {
   stroke(255,0,0);
   noFill();
 
-  stroke(255);
+  stroke(35);
   line(10, 10, 90, 10);
 
-    stroke(255);
+    stroke(255, 90, 60);
     rect(20, 20, 80, 50);
 
-    stroke(255);
+    stroke(255, 180, 90);
     ellipse(320, 240, 100, 50);
 }
