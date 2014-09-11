5305e3c2f82f43e31c80e5da13b02c06b327c23f
heesuchoi1 <heesu7563@gmail.com>
2014-09-11 13:38:22 +0900

    sdtryuiuo

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 function setup() {
   createCanvas(640, 480);
 
-
  //0: black, 255:white
  //RGB	
   background(255, 0, 0);
+  
   stroke(255);
   noFill();
+
   line(10, 10, 90, 10);
     rect(20, 20, 80, 50);
     ellipse(320, 240, 100, 50);
